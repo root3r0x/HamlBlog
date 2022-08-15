118f6cf531ab3673fe016cf60c640a396cb46663
Migue LD <ledesma.duarte@duck.com>
2022-08-14 19:03:05 -0600

    Ruta index a Students

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'student/index'
+  get 'student/add_student'
+  get 'student/edit_student'
+  get 'student/delete_student'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'student#index'
 end
